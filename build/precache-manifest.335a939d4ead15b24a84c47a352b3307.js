fea1c32e29f7518de1f4e9ecdcb67f09951f2234
4lexRossi <devalexrossi@gmail.com>
2020-07-15 18:41:24 -0300

    adicional changes

---
+++ build/precache-manifest.335a939d4ead15b24a84c47a352b3307.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e466b0f65c9b4087b29038c6f282ca7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba0b113d65e93bcc3910",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1e5b4d757ba995b2238f",
+    "url": "/static/css/main.99a206c4.chunk.css"
+  },
+  {
+    "revision": "ba0b113d65e93bcc3910",
+    "url": "/static/js/2.30e6ba1c.chunk.js"
+  },
+  {
+    "revision": "a35346a5ff3029a3521395d4c125dfed",
+    "url": "/static/js/2.30e6ba1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5b4d757ba995b2238f",
+    "url": "/static/js/main.8bf64106.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "a472dff0fbb47a3c4ce9983e2620c9c2",
+    "url": "/static/media/background.a472dff0.jpg"
+  },
+  {
+    "revision": "91a960a04d76d57b14dbe3a6d25176ab",
+    "url": "/static/media/camera.91a960a0.png"
+  }
+]);
